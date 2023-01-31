567afebfabab10ca9680502246ad237ee6875a05
Clara <c.garcia@cfa-insta.fr>
2023-01-31 20:38:34 +0100

    database ok

--- sql/flux_es.sql
+++ sql/flux_es.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 26 jan. 2023 à 15:49
+-- Généré le : mar. 31 jan. 2023 à 19:37
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -30,11 +30,20 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tsp`;
 CREATE TABLE IF NOT EXISTS `tsp` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cuid` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
   `prenom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `nom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `mdp` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=55 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `tsp`
+--
+
+INSERT INTO `tsp` (`id`, `cuid`, `prenom`, `nom`, `mdp`) VALUES
+(54, 'HGTJ1285', 'Victoria', 'DOUGLAS', '0808'),
+(53, 'CYGP3115', 'Clara', 'GARCIA', '0909');
 
 -- --------------------------------------------------------
 
@@ -47,13 +56,24 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `prenom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `nom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `cu-id` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
-  `heure` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `cuid` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `heure` time(4) NOT NULL,
   `motif` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `numero` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `commentaire` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `prisencharge` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `prenom`, `nom`, `cuid`, `heure`, `motif`, `numero`, `commentaire`, `prisencharge`) VALUES
+(13, 'Clara', 'GARCIA', 'dd', '14:15:00.0000', 'Incident', 'dad', 'adad', 1),
+(14, 'ezfezf', 'zefzef', 'fzef', '16:43:00.0000', 'Incident', 'zefezf', 'zefezf', 1),
+(15, 'cdc', 'sc', 'sc', '17:43:00.0000', 'Autre', 'scsc', 'scsc', 1),
+(16, 'ze', 'ze', 'ze', '17:12:00.0000', 'DMI', 'ze', 'ze', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
